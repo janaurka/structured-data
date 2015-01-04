90a6ae9a9f96c98d0c53e851d8d0d194e0cf1a73
jan <jan-github@aurka.com>
2015-01-04 01:03:24 +0100

    Done until maps

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,21 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  "Sums the first and the third opject of a vector -> if there are to less options, there will be a NullPointerException"
+  (let [first (get v 0)
+        second (get v 2)]
+    (+ first second)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[first second third] v]
+    (+ first third)))
 
 (defn point [x y]
   [x y])
@@ -19,22 +24,37 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        width (- x1 x2)]
+    (if (pos? width)
+      width
+      (* -1 width))))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        height (- y1 y2)]
+    (if (pos? height)
+      height
+      (* -1 height))))
 
 (defn square? [rectangle]
-  :-)
+    (= (height rectangle)
+       (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle)
+     (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [px py] point]
+    (and (<= x1 px x2)
+          (<= y1 py y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (and (contains-point? outer p1)
+         (contains-point? outer p2))))
 
 (defn title-length [book]
   :-)
